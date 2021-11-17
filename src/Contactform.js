bb109c0718e8258fd3d33c6a1f34aabc97541237
akku19 <akashsen127@gmail.com>
2021-11-17 19:09:02 +0530

    add firebase

--- src/Contactform.js
+++ src/Contactform.js
@@ -1,11 +1,25 @@
 import { method } from 'lodash';
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Contactform = () => {
     const [state, setstate] = useState({
         name:'',
         email:''
     })
+    const [userdata, setuserdata] = useState([]);
+
+   const getuserdata = useEffect( async () => {
+    fetch(
+        "https://contactform-285e1-default-rtdb.firebaseio.com/akash.json")
+                    .then((res) => res.json())
+                    .then((json) => {                        
+                        const entries = Object.entries(json);
+                        setuserdata(entries);
+                         console.log(entries);
+                    })
+          
+        
+    }, [])
 
     let name,value;
 
@@ -13,13 +27,12 @@ const Contactform = () => {
     name= e.target.name;
     value = e.target.value;
     setstate({...state, [name]:value});
-    console.log(state);
+    //console.log(state);
     }
 
     const submitdata = async (e)=>{
         e.preventDefault();
-        //const[name,email] = state;
-        //console.log(`name ${name} and email is ${email}`);
+      
         const data = await fetch('https://contactform-285e1-default-rtdb.firebaseio.com/akash.json',
         {
             method:"POST",
@@ -29,9 +42,50 @@ const Contactform = () => {
             body:JSON.stringify({name:state.name,email:state.email})
            }
         );
+        if(data.status=='200'){
+            alert('Data recived');
+            //setuserdata({...state, [name]:value});
+            fetch(
+                "https://contactform-285e1-default-rtdb.firebaseio.com/akash.json")
+                            .then((res) => res.json())
+                            .then((json) => {                        
+                                const entries = Object.entries(json);
+                                setuserdata(entries);
+                                 console.log(entries);
+                            })
+        }
     }
     return (
         <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                    <table className="table">
+  <thead>
+    <tr>
+
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+  {userdata.map((user,keys) => (
+      <>
+      {console.log(user.name)}
+        <tr key={keys}>     
+        <td>{user[1].name} </td>
+        <td>{user[1].email}</td>
+      </tr>
+      </>
+      ))}
+    
+  
+
+  </tbody>
+</table>
+                    </div>
+                </div>
+            </div>
             <form method="POST" >
                 <div className="container">
                     <div className="row">
@@ -44,8 +98,7 @@ const Contactform = () => {
                         <input type="email" name="email" placeholder="Enter your Name" value={state.email}  onChange={getdata}/>
                     </div>
 
-                    <div className="row">
-                        
+                    <div className="row">                        
                         <input type="submit" name="submitform"  onClick={submitdata}/>
                     </div>
 
